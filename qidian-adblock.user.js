449269ea7481916837b9df3c75b07700e2f0dab1
alkumaish <alkumaish@github.com>
2017-10-19 18:22:34 +0300

    Now the scrip works fine with both www.webnovel.com and m.webnovel.com.

--- qidian-adblock.user.js
+++ qidian-adblock.user.js
@@ -123,14 +123,121 @@ function main() {
   });
 }
 
+function m_main() {
+  Array.from(
+    // Locked chapters.
+    document.querySelectorAll('.j_chapterWrapper')
+  ).forEach((lock) => {
+    // Remove this class so this chapter won't be processed the next time
+    // `main` is called.
+    lock.classList.remove('j_chapterWrapper');
+    
+    // Element with the chapter content.
+    const contentElement = lock.querySelector('.j_chaTxt.cha-txt.cha-txt-hide');
+    contentElement.classList.remove('cha-txt-hide');
+    
+    contentElement.style.opacity = '0.1';
+    
+    // Get the ID for the series ("book").
+    //
+    // Some chapters have the `data-bid` property, but not all of them.
+    // That's why it's better to just get this from the URL.
+    const bid = window.location.href.split('/book/')[1].split('/')[0];
+    
+    // Get the ID for the chapter.
+    const cid = window.location.href.split('/book/')[1].split('/')[1];
+    
+    // Both ID are required.
+    if (!bid || !cid) {
+      return;
+    }
+
+    var csrfTok;
+    var x = document.cookie.split(';'); 
+    for (var i = 0; i < x.length; i++) { 
+	    var c = x[i]; 
+	    c = c.trim();
+	    if (c.startsWith("_csrf")) csrfTok = c; 
+    }
+    
+    return fetch(
+		    `https://m.webnovel.com/ajax/chapter/getChapterContentToken?${csrfTok}&bookId=${bid}&chapterId=${cid}`
+    )
+      .then(resp => resp.json())
+      .then(data => data.data.token)
+      .then(token => encodeURIComponent(token))
+      .then(token => new Promise((resolve) => {
+        // The raw body of the chapter.
+        //
+        // It will be plain text, so we must manually build the HTML for it.
+        let content = '';
+
+        // Try to get the content of the chapter, and fulfill the promise once
+        // we have it.
+        //
+        // This function will retry until it succeeds.
+        function tick() {
+          const url = `https://m.webnovel.com/ajax/chapter/getChapterContentByToken?${csrfTok}&token=${token}`;
+
+          fetch(url)
+            .then(resp => resp.json())
+            .then((data) => {
+              content = data.data.content.trim();
+
+              if (content) {
+                resolve(content);
+              } else {
+                setTimeout(tick, INTERVAL_TOKEN_CHECK);
+              }
+            })
+            .catch((err) => {
+              console.error(err.stack);
+
+              tick();
+            });
+        }
+
+        tick();
+      }))
+      .then((content) => {
+        // Build the HTML for the chapter content.
+        //
+        // For now we only split on line breaks and wrap each piece
+        // with "<p></p>" tags.
+        const chapterHtml = content
+          .split('\n')
+          .map(p => p.trim())
+          .filter(p => !!p)
+          .map(p => `<p>${p}</p>`)
+          .join('');
+
+        // Update the chapter content and turn opacity back to 100%.
+        contentElement.innerHTML = chapterHtml;
+        contentElement.style.opacity = '1';
+      })
+      .catch((err) => {
+        console.error(err.stack);
+      });
+  });
+}
+
+
 // Since Qidian may load new chapters without refreshing the page, we must
 // continuously check for new chapters in the page.
 if (document.location.href.startsWith('https://www.webnovel.com/book')) {
 	setInterval(main, INTERVAL_CHAPTER_CHECK);
 }
 
+if (document.location.href.startsWith('https://m.webnovel.com/book')) {
+	setInterval(m_main, INTERVAL_CHAPTER_CHECK);
+}
+
 if (document.location.href.startsWith('https://www.webnovel.com/rssbook/')) {
 	if (g_data.url) {
 		window.location.replace(g_data.url);
 	}
+}
+
+if (document.location.href.startsWith('https://m.webnovel.com/rssbook/')) {
+	window.location.replace(document.location.href.replace("rssbook","book"));
 }
